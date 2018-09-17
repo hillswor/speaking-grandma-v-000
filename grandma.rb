77c764cc2f1050eda856f43239b5192a4cbe84c2
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-17 14:12:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
